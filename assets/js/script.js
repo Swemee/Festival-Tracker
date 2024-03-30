dee114925839afe6bae5119b54a18ea987698c49
Swemee <sweme1@gmail.com>
2024-03-29 19:30:36 -0500

    Restructured the Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,27 +13,8 @@ let selectedEvents = document.getElementById("selectedEvents");
 // Reset button
 let resetBtn = document.getElementById("resetBtn");
 
-let location = document.getElementById("location").value;
-let date = document.getElementById("date").value;
 
-    // Fetch events from Ticketmaster API
-    fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=vPcBytlSIXMMaAICmG6wPiMssr9MxzF7&city=${encodeURIComponent(location)}&startDateTime=${date}T00:00:00Z`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data._embedded && data._embedded.events && data._embedded.events.length > 0) {
-                displayEvents(data._embedded.events);
-            } else {
-                displayNoEventsFound();
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+
 
 // Open modal
 openModalBtn.onclick = function() {
@@ -56,6 +37,29 @@ resetBtn.onclick = function() {
 // Event form submission
 eventForm.onsubmit = function(event) {
     event.preventDefault(); // Prevent default form submission
+
+    let location = document.getElementById("location").value;
+    let date = document.getElementById("date").value;
+
+        // Fetch events from Ticketmaster API
+        fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=vPcBytlSIXMMaAICmG6wPiMssr9MxzF7&city=${encodeURIComponent(location)}&startDateTime=${date}T00:00:00Z`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data._embedded && data._embedded.events && data._embedded.events.length > 0) {
+                displayEvents(data._embedded.events);
+            } else {
+                displayNoEventsFound();
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+
 }
 // Display events on the main page
 function displayEvents(events) {
